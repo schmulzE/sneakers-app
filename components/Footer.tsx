40020627a0d02098d6ed07212efdab9814b6b992
schmulz eshiet <wisdomeshiet65@gmail.com>
2022-09-21 00:58:35 +0100

    created main layout

---
+++ components/Footer.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+   <footer className="">
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <span className="">
+          </span>
+        </a>
+      </footer>
+  )
+}
+
+export default Footer
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import React,{ReactElement} from 'react'
+import Navbar from "../components/Navbar"
+import Footer from "../components/Footer"
 
+
+function Layout({children }: {children: ReactElement}) {
+  return(
+    <>
+    <Navbar/>
+      <main>{children}</main>
+    <Footer/>  
+    </>
+  )
+}
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Layout><Component {...pageProps} /></Layout> 
 }
 
 export default MyApp
