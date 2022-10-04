c9591bde437ffc111493690b3af3c1c590127a5c
schmulz eshiet <wisdomeshiet65@gmail.com>
2022-10-04 17:49:20 +0100

    created modal using chakra ui component

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,9 +1,49 @@
 import React from 'react'
+import { IconContext } from "react-icons";
+import { BsFilter } from "react-icons/bs";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  Button,
+} from '@chakra-ui/react'
 
-const Modal = () => {
+
+
+const Modals: React.FC = () => {
+  const { onOpen,isOpen, onClose } = useDisclosure()
   return (
-    <div>Portal</div>
+    <>
+    <IconContext.Provider value={{ className: "global-class-name h-8 w-8", }}>
+      <button onClick={onOpen} className='capitalize flex justify-center align-center bg-st my-4 py-1 outline-none border border-slate-800 rounded block w-full'>
+        <BsFilter/>
+        <span className='text-xl font-medium'>Filter</span>
+      </button> 
+    </IconContext.Provider>
+
+      <Modal size={'full'} blockScrollOnMount={false} isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            Hi there!
+          </ModalBody>
+
+          {/* <ModalFooter>
+            <Button colorScheme='blue' mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant='ghost'>Secondary Action</Button>
+          </ModalFooter> */}
+        </ModalContent>
+      </Modal>
+    </>
   )
 }
-
-export default Modal
+export default Modals
