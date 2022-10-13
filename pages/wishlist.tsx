f1b73950d9fb7f39d90f58980c81efcf02d86a11
schmulz eshiet <wisdomeshiet65@gmail.com>
2022-10-13 22:44:44 +0100

    created button to sign up page; display wislisted product via dynamic cart components

--- pages/wishlist.tsx
+++ pages/wishlist.tsx
@@ -1,21 +1,37 @@
 import type { NextPage } from 'next'
-import React, {useEffect, useState} from "react";
-// import Cart from "../components/Cart"
+import Link from 'next/link'
+import React, { useContext, useState } from "react";
+import WishlistContext from "../store/wishlist_context";
+import { MdClose } from "react-icons/md";
+import Form from "../components/Form";
+import Cart from "../components/Cart"
 
 const Wishlist: NextPage = () => {
-  const [wishlist, setWishlist] = useState<any[]>([])
+  const [showButton, setShowButton] = useState(true)
+  const wishlistCtx = useContext(WishlistContext)
+  const numberOfItems = wishlistCtx.items.length
+  
+  const handleClick = (item: { id: number; }) => {
+    wishlistCtx.removeItem(item.id)
+  }
 
-  useEffect(() => {
-  const wishlistData = JSON.parse(localStorage.getItem('wishlist')!);
-  if (wishlistData) {
-   setWishlist(wishlistData);
-  } 
-  }, [])
-  console.log(wishlist)
   return (
-    <div>
-      <h2>wishlist</h2>
-      {/* <Cart/> */}
+    <div className='pt-12'>
+      <h1 className='font-bold tracking-wide uppercase text-center text-3xl'>wishlist</h1>
+      <h3 className='uppercase text-lg text-center'>{numberOfItems} pieces</h3>
+
+      <div className="px-4">
+        <p className='text-center pt-4'>keep track of all your favourite pieces, no matter your browser or device, all in one place</p>
+        
+        <Link href={{ pathname: 'men/sneakers' }}>
+          <a className='uppercase bg-black text-white px-8 my-4 py-2 outline-none border border-slate-800 rounded block w-full text-center'>
+            sign up
+          </a>
+        </Link>
+
+        <Cart data={wishlistCtx.items} handleClick={handleClick} showButton={showButton} icon={<MdClose />} wishlist={[]}/>
+        <Form/>
+      </div>
     </div>
   )
 }
