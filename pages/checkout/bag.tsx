767a7a40cf57dcf96fbb622f63b7a77b182cab3e
schmulz eshiet <wisdomeshiet65@gmail.com>
2022-10-21 15:23:56 +0100

    implemented summary table

--- pages/checkout/bag.tsx
+++ pages/checkout/bag.tsx
@@ -1,19 +1,23 @@
 import type { NextPage } from 'next'
-import React, { useState } from 'react';
+import React, { useState , useContext} from 'react';
+import BagContext from "../../store/bag_context";
 import Link from 'next/link';
 import Modal from '../../components/Modal';
-import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react';
 import AuthForm from '../../components/AuthForm'
+import { IconContext } from "react-icons";
 import { FcGoogle } from "react-icons/fc";
 import { FiInfo } from "react-icons/fi";
-// import { AiOutlineEyeInvisible, AiOutlineEye } from "react-icons/ai";
-import { IconContext } from "react-icons";
+import { BiPhone } from "react-icons/bi";
+import { AiOutlineEyeInvisible, AiOutlineEye, AiOutlineMail, AiOutlineQuestionCircle } from "react-icons/ai";
 import { useSession, signIn, signOut } from "next-auth/react"
+import Form from '../../components/Form'
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react';
 
 
 
 const Bag: NextPage = () => {
  const [popUp, setPopUp] = useState(false)
+ const bagCtx = useContext(BagContext)
  const [form, setForm] = useState({
   firstName: '',
   lastName: '',
@@ -31,11 +35,11 @@ const Bag: NextPage = () => {
   return (
     <div className='mx-4'>
     <div className='mt-3 mb-4'>
-      <div className="text-2xl text-center uppercase font-bold">shopping bag</div>
+      <div className="text-3xl text-center uppercase font-bold">shopping bag</div>
       <p className="text-center mt-2">Never lose styles in your shopping bag again - simply login or register to save them</p>
     </div>
 
-    <button onClick={modalHandler} className='capitalize bg-black text-white px-8 my-4 py-2 outline-none border border-slate-800 rounded-lg block w-full text-center'>
+    <button onClick={modalHandler} className='font-semibold capitalize bg-black text-white px-8 my-4 py-2 outline-none border border-slate-800 rounded-lg block w-full text-center'>
       go to checkout
     </button>
 
@@ -52,9 +56,84 @@ const Bag: NextPage = () => {
         </li>
       </ul>
 
-      <hr/>
-      <p>yo!</p>
+      {/* <hr/> */}
+      <div id="table-section">
+        <h2 className='font-bold'>Summary</h2>
+        <table className="w-full text-md text-left text-gray-500 table-fixed">
+          <tbody>
+            <tr className="bg-white">
+              <th scope="row" className="pb-1 pt-3 font-medium text-gray-900 whitespace-nowrap">
+                  Subtotal
+              </th>
+            
+              <td className="py-1 text-right">
+                  $280
+              </td>
+            </tr>
+            <tr className="bg-white pb-4">
+              <th scope="row" className="pt-1 pb-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                  Delivery
+              </th>
+              <td className="pt-1 pb-3 text-right">
+                  $19
+              </td>
+                
+            </tr>
+            <tr className="bg-white border-t">
+              <th scope="row" className="py-4 font-medium text-gray-900 whitespace-nowrap">
+                Total
+              </th>
+              <td className="py-4 text-right">
+                  $299
+              </td>      
+            </tr>
+          </tbody>
+        </table>
+      </div>
 
+      <Link href='/checkout'>
+        <a onClick={modalHandler} className=' font-semibold capitalize bg-black text-white px-8 my-4 py-2 outline-none border border-slate-800 rounded-lg block w-full text-center'>
+            go to checkout
+        </a>
+      </Link>
+
+      <div id="poster1" className='bg-neutral-100 text-left py-6 px-3 my-3 w-full'>
+        <h2 className='pb-3 uppercase'>free returns</h2>
+        <p>Free returns within 14 days(exludes final sales, delivery charges)</p>
+      </div>
+
+      <h1>Recommendations</h1>
+
+      <div id="poster2" className='bg-neutral-100 text-left py-6 px-3 my-3 w-full'>
+        <h2 className='pb-3 capitalize font-semibold'>need help?</h2>
+        <p className='text-sm'>Contact our global Customer Service team, you can reach us by phone or email. Alternatively, you may find the answer in the <a className='underline'>Frequently Asked Question page.</a></p>
+        <div className="flex flex-wrap py-4">
+          <div id="flex-1" className='py-3'>
+            <span className="font-semibold text-lg flex ">
+              <BiPhone size={25}/>
+              <h2 className='pl-4'>Call us</h2>
+              </span>
+            <p className='underline py-2 text-sm font-bold'>+1 6573 9487</p>
+            <p className='text-sm'>Available Monday to Friday, 18:00 - 21:00 EST</p>
+          </div>
+          <div id="flex-2" className='py-3'>
+            <span className="font-semibold text-lg flex ">
+              <AiOutlineMail size={25}/>
+              <h2 className='pl-4'>Email us</h2>
+              </span>
+            <p className='text-sm underline py-2'>Click here to send us an email</p>
+          </div>
+          <div id="flex-3" className='py-3'>
+            <span className="font-semibold text-lg flex ">
+              <AiOutlineQuestionCircle size={25}/>
+              <h2 className='pl-4'>FAQs</h2>
+              </span>
+            <p className='text-sm py-2'>Find the answers you need in our FAQs</p>
+          </div>
+          
+        </div>
+      </div>
+    <Form/>
     <Modal onOpen={popUp} setPopUp={setPopUp}>
       <Tabs>
         <TabList>
