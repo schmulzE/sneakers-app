d1d7c70c503d602b40074edb6b57661814814909
schmulz eshiet <wisdomeshiet65@gmail.com>
2022-11-13 20:06:31 +0100

    implemented authentication form

---
+++ components/AuthForm.tsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+
+interface Props{
+  children: React.ReactNode
+}
+
+const AuthForm: React.FC<Props> = ({ children }) => {
+  const [value, setValue] = useState('');
+  const onChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+    setValue(event.target.value);
+  };
+  return (
+   <form>
+    {children}
+  </form>
+  )
+}
+
+export default AuthForm
