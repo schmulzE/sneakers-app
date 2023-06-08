1501ce5836733dab40c6df5b54ce60dc6c240a3b
wisdom eshiet <wisdomeshiet65@gmail.com>
2023-06-08 09:21:53 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      screen: {
+        sm: "576px",
+        // => @media (min-width: 576px) { ... }
+
+        md: "960px",
+        // => @media (min-width: 960px) { ... }
+
+        lg: "1440px",
+        // => @media (min-width: 1440px) { ... }
+      },
+    },
+  },
+  corePlugins: {
+    aspectRatio: false,
+  },
+  plugins: [require("@tailwindcss/aspect-ratio")],
+};
