000d75f82a2755d2fcd774bbcfff0229e7da96db
wisdom eshiet <wisdomeshiet65@gmail.com>
2023-06-14 15:33:39 +0100

    added comments

--- lib/validate.ts
+++ lib/validate.ts
@@ -1,4 +1,4 @@
-const loginValidate = (values: any) => {
+export const loginValidate = (values: any) => {
   type Errors = {
     email: string,
     password: string
@@ -9,13 +9,14 @@ const loginValidate = (values: any) => {
     password: ''
   }
 
-
+//validate email
   if (!values.email) {
     errors.email = 'Required';
   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
     errors.email = 'Invalid email address';
   }
- 
+  
+ //validate password
   if(!values.password){
     errors.password = 'Required';
   }else if (values.password.length < 8 || (values.password.length > 20)) {
@@ -63,5 +64,3 @@ export const registerValidate = (values: any) => {
 
   return errors;
 }
-
-export default loginValidate
