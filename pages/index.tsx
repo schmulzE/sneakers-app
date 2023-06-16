15ec87fca3e7896951c8e7abaad27317fce12335
wisdom eshiet <wisdomeshiet65@gmail.com>
2023-06-16 10:59:50 +0100

    gsap animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from 'next'
-import { useContext, useCallback } from "react";
+import { useContext, useCallback, useEffect, useLayoutEffect, useRef } from "react";
 import Link from 'next/link'
 import Image from "next/image";
 import { IconContext } from "react-icons";
@@ -14,7 +14,7 @@ import Form from "../components/Form";
 import options from '../client_api/options';
 import WishlistContext from "../store/wishlist_context";
 import { motion, Variants } from "framer-motion"
-// import Banner from "../components/Banner"
+import {gsap} from "gsap"
 
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -53,30 +53,6 @@ import Slides from '../components/Slides';
   },
  ]
 
- const scrollVariants: Variants = {
-  offscreen: {
-    y: -100
-  },
-  onscreen: {
-    y: 5,
-    transition: {
-      type: "spring",
-      bounce: 0.4,
-      duration: 3
-    }
-  },
-  visible: {
-      color: ["#000","#60F", "#09F", "#FA0", "#A06", "#0E4", "#30E"],
-      transition: {
-        duration: 10,
-        repeat: Infinity,
-        repeatType: "reverse",
-        ease: [0.075, 0.82, 0.165, 1],
-        delay: 5
-      }
-  }
- }
-
  const colorVariants: Variants = {
     visible: {
       color: ["#000","#60F", "#09F", "#FA0", "#A06", "#0E4", "#30E"],
@@ -90,65 +66,64 @@ import Slides from '../components/Slides';
     }
   };
 
-type MenSneakersPageProps = {
-  menSneakers: any[]
-  womenSneakers: any[]
-}
-const Home: NextPage<MenSneakersPageProps> = ( { menSneakers, womenSneakers }) => {
-  const wishlistCtx = useContext(WishlistContext)
-
-  const wishlist = wishlistCtx.items
-
-
-  const wishlisted = useCallback(
-  (item : any) =>  wishlist.findIndex((it: { id: any; }) => it.id == item.id) != -1
-    ,
-  [wishlist],
-  )
+  
+  type MenSneakersPageProps = {
+    menSneakers: any[]
+    womenSneakers: any[]
+  }
+  const Home: NextPage<MenSneakersPageProps> = ( { menSneakers, womenSneakers }) => {
+    const wishlistCtx = useContext(WishlistContext)
+    const sectionTwo = useRef()    
+    const wishlist = wishlistCtx.items
+    
+    
+    const wishlisted = useCallback(
+      (item : any) =>  wishlist.findIndex((it: { id: any; }) => it.id == item.id) != -1
+      ,
+      [wishlist],
+      )
+      
+      useLayoutEffect(() => {
+        const ctx = gsap.context(() => {
+          const tl = gsap.timeline({
+            scrollTrigger: {
+              trigger: sectionTwo.current,
+              start: 'top center',
+              toggleActions: "restart none none none"
+            }
+          });
+          tl.from('.text-block',{ y: -60, duration: 1, ease: 'Power2.easeIn'})
+          tl.from('#image-1',{ y: 80, duration: 1, ease: 'Power2.easeIn'})
+        })
+        return () => ctx.revert()
+      }, [])
 
   const handleClick = (item: { id: number; }) => {
-   const foundItem =  wishlistCtx.items.find((it: { id: number; }) => it.id === item.id )
-   if(foundItem) {
-    wishlistCtx.removeItem(item.id)
-   }else {
-    wishlistCtx.addItem(item)
-   }
+    const foundItem =  wishlistCtx.items.find((it: { id: number; }) => it.id === item.id )
+    if(foundItem) {
+      wishlistCtx.removeItem(item.id)
+    }else {
+      wishlistCtx.addItem(item)
+    }
   }
 
   return (
     <>
-    <div className='container px-4 md:mt-7'>
-      <motion.div initial={{ y: -400}} animate={{ y: 5 }} transition={{ type: "spring", bounce: 0.5, duration: 1, }} id="banner-text" className='mt-6 ml-12'>
-        <h3  className='text-lg md:text-2xl md:font-extrabold font-bold uppercase'>fall / winter</h3>
-        <h1 className="uppercase text-2xl font-bold md:text-6xl md:font-extrabold md:mb-14 md:mt-8">new season drop</h1>
-        <div id='button' className='my-6'>
-          <Link href={''}>
-            <a className='p-2 border-4 md:px-14 md:text-sm border-neutral-900 uppercase text-xs font-bold border-r-0'>shop men</a>
-          </Link>
-          <Link href={''}>
-            <a className='p-2 border-4 md:px-14 md:text-sm border-neutral-900 uppercase text-xs font-bold'>shop women</a>
-          </Link>
-        </div>
-      </motion.div>
-    
-        <Slides/>
-
-    </div>
+    <Slides/>
    
-    <div id="section-2" className='mt-10 relative px-4 py-64'>
-      <motion.div variants={scrollVariants} initial={"offscreen"} whileInView={"onscreen"} viewport={{ once: true, amount: 0. }}  className="border-4 border-neutral-900 py-10 px-14 w-64 text-left absolute top-0 right-14 z-10 md:right-10">
-          <span  className="block text-lg font-bold uppercase">the concept</span>
-          <h3 className="uppercase font-bold text-2xl">
+    <div id="section-2" className='mt-10 relative px-14 py-64' ref={sectionTwo}>
+      <div className="text-block hover:bg-black hover:text-white border-4 border-neutral-900 py-10 px-14 w-64 text-left absolute top-0 right-14 lg:right-[12%] lg:w-[350px] z-10 md:right-10">
+          <span  className="block text-lg lg:text-xl font-bold lg:font-black uppercase">the concept</span>
+          <h3 className="uppercase font-bold text-2xl lg:text-5xl">
             minimal frames intense colors dynamic lifestyle
           </h3>
-      </motion.div>
-      <div id="image-1" className="absolute bottom-0">
-        {/* <Image src="/Cromier-HP-Banner-Concept.jpg" alt="" width={345} height={245} /> */}
-        <Image src="/Cromier-HP-Banner-Concept.jpg" alt="" width={516} height={367} />
+      </div>
+      <div id="image-1" className="absolute top-0" >
+        <img src="/Cromier-HP-Banner-Concept.jpg" className='max-w-[800px] lg:mt-20'/>
       </div>
     </div>
   
-     <div className='px-4 grid md:grid-cols-2'>
+     <div className='px-4 grid md:grid-cols-2 mt-64 lg:mt-[500px] relative'>  
      <Swiper
         modules={[ Pagination, Autoplay ]}
         spaceBetween={50}
@@ -156,7 +131,7 @@ const Home: NextPage<MenSneakersPageProps> = ( { menSneakers, womenSneakers }) =
         loop
         autoplay
         pagination={{ clickable: true }}
-        className=" w-52 md:w-68 md:my-0 md:mt-28 my-8"
+        className=" w-52 lg:w-96 md:w-68 md:my-0 md:mt-28 my-8 lg:absolute lg:-top-28 lg:left-[5%]"
         >
         {menSneakers.slice(0, 10).filter((item: { id: number }) =>  item.id).map((product: any)=> (
           <SwiperSlide key={product.id}>
@@ -189,21 +164,21 @@ const Home: NextPage<MenSneakersPageProps> = ( { menSneakers, womenSneakers }) =
         </Swiper>
         
         <div id="section-4" className='relative py-64 px-4'>
-          <div className="text-right w-48 absolute top-0 md:top-48 md:-left-10 z-10">
+          <div className="text-right w-48 absolute top-0 md:top-48 md:-left-10 lg:-top-16 lg:-left-12 z-10">
             <motion.span variants={colorVariants} animate="visible" className="uppercase font-bold">fw22-23</motion.span>
-            <motion.div variants={colorVariants} animate="visible" className="uppercase font-bold text-3xl md:text-5xl md:break-normal md:flex md:justify-end">men<br className='mr-12'/>collection</motion.div>
+            <motion.div variants={colorVariants} animate="visible" className="uppercase font-bold text-3xl md:text-5xl lg:text-5xl md:break-normal md:flex md:justify-end">men<br className='mr-12'/>collection</motion.div>
             <Link href={''}>
               <a className='my-4 py-1 bg-white text-center block border-4 border-neutral-900 uppercase text-lg font-bold'>shop now</a>
             </Link>
           </div>
           <div id="image" className='absolute bottom-0 right-4 pt-4'>
-            <Image src="/men.jpg" alt="" width={258} height={389}/>
+            <img src={"/men.jpg"} className='max-w-[600px]'/>
           </div>
         </div>
       </div>
 
 
-    <div className='px-2 grid md:grid-cols-2 mt-28 mb-[36px]'>
+    <div className='px-2 grid md:grid-cols-2 mt-64 mb-[36px]'>
        <Swiper
         modules={[ Pagination, Autoplay ]}
         spaceBetween={50}
@@ -211,7 +186,7 @@ const Home: NextPage<MenSneakersPageProps> = ( { menSneakers, womenSneakers }) =
         loop
         autoplay
         pagination={{ clickable: true }}
-        className="w-52 md:w-68 my-8 md:my-0 md:mt-28 order-last"
+        className="w-52 md:w-68 my-8 md:my-0 md:mt-28 order-last lg:w-[450px] lg:flex lg:flex-col lg:justify-center lg:self-center"
         >
         {womenSneakers.slice(0, 10).filter((item: { id: number }) =>  item.id).map((product: any)=> (
           <SwiperSlide key={product.id}>
@@ -230,7 +205,7 @@ const Home: NextPage<MenSneakersPageProps> = ( { menSneakers, womenSneakers }) =
                 <img
                   src={product.images?.cutOut}
                   alt={product.imageAlt}
-                  className="h-full w-full object-cover object-center group-hover:opacity-75 p-3"
+                  className="h-full w-full object-contain object-center group-hover:opacity-75 p-3"
                 />
               </div>
               <h3 className="mt-4 text-sm text-gray-700 font-medium">{product.brand?.name}</h3>
@@ -244,15 +219,15 @@ const Home: NextPage<MenSneakersPageProps> = ( { menSneakers, womenSneakers }) =
         </Swiper>
 
       <div id="section-5" className='relative py-64 order-1'>
-        <div className="text-left w-48 absolute top-0 right-0 z-10 md:top-48 md:-right-10 ">
+        <div className="text-left w-48 absolute top-0 right-0 z-10 md:top-48 md:-right-10 lg:top-24 lg:right-9">
           <motion.span variants={colorVariants} animate="visible" className="uppercase font-bold">fw22-23</motion.span>
-          <motion.div variants={colorVariants} animate="visible" className="uppercase font-bold text-3xl md:text-5xl md:break-normal">women<br className='my-1'/>collection</motion.div>
+          <motion.div variants={colorVariants} animate="visible" className="uppercase font-bold text-3xl lg:text-4xl md:text-5xl md:break-normal">women<br className='my-1'/>collection</motion.div>
         <Link href={''}>
           <a className='my-4 py-1 bg-white text-center block border-4 border-neutral-900 uppercase text-lg font-bold'>shop now</a>
         </Link>
         </div>
         <div id="image" className='absolute bottom-0 left-4 pt-4'>
-          <Image src="/women.jpg" alt="" width={258} height={389}/>
+          <img src='/women.jpg' className='max-w-[500px]'/>
         </div>
       </div>
     </div>
