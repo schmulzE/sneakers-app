4fd445f69a95de1068844399ca8bc250460b650d
wisdom eshiet <wisdomeshiet65@gmail.com>
2023-09-13 22:08:56 +0100

    refactoring: inserted new context api code & hid api url

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,9 +12,9 @@ import { BsTag, BsQuestionCircle, BsSuitHeart, BsSuitHeartFill} from 'react-icon
 import { FaRegComment } from 'react-icons/fa'
 import Form from "../components/Form";
 import options from '../client_api/options';
-import WishlistContext from "../store/wishlist_context";
 import { motion, Variants } from "framer-motion"
 import {gsap} from "gsap"
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -26,9 +26,8 @@ import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import 'swiper/css/autoplay';
 import 'swiper/css/effect-fade'
-
-
 import Slides from '../components/Slides';
+import { useCart } from '../context/CartContext';
 
  const banner: {}[] = [
   {
@@ -65,45 +64,42 @@ import Slides from '../components/Slides';
       }
     }
   };
-
+  
+  // gsap.registerPlugin(ScrollTrigger)
   
   type MenSneakersPageProps = {
-    menSneakers: any[]
-    womenSneakers: any[]
+    menSneakers: Sneakers[]
+    womenSneakers: Sneakers[]
   }
   const Home: NextPage<MenSneakersPageProps> = ( { menSneakers, womenSneakers }) => {
-    const wishlistCtx = useContext(WishlistContext)
-    const sectionTwo = useRef()    
-    const wishlist = wishlistCtx.items
-    
+  const sectionTwo = useRef()    
+  const {addItem, removeItem, items} = useCart()
     
     const wishlisted = useCallback(
-      (item : any) =>  wishlist.findIndex((it: { id: any; }) => it.id == item.id) != -1
+      (item: Sneakers) =>  items.findIndex((it: { id: number; }) => it.id == item.id) != -1
       ,
-      [wishlist],
-      )
+      [items],
+    )
+    useEffect(() => {
+      const ctx = gsap.context(() => {
+        const tl = gsap.timeline({
+          scrollTrigger: {
+            trigger: sectionTwo.current,
+            start: 'top center',
+          }
+        });
+        tl.from('.text-block',{ y: -60, duration: 1, ease: 'Power2.easeIn'})
+        tl.from('#image-1',{ y: 80, duration: 1, ease: 'Power2.easeIn'})
+      })
+      return () => ctx.revert()
+    }, [])
       
-      useLayoutEffect(() => {
-        const ctx = gsap.context(() => {
-          const tl = gsap.timeline({
-            scrollTrigger: {
-              trigger: sectionTwo.current,
-              start: 'top center',
-              toggleActions: "restart none none none"
-            }
-          });
-          tl.from('.text-block',{ y: -60, duration: 1, ease: 'Power2.easeIn'})
-          tl.from('#image-1',{ y: 80, duration: 1, ease: 'Power2.easeIn'})
-        })
-        return () => ctx.revert()
-      }, [])
-
   const handleClick = (item: { id: number; }) => {
-    const foundItem =  wishlistCtx.items.find((it: { id: number; }) => it.id === item.id )
+    const foundItem =  items.find((it: { id: number; }) => it.id === item.id )
     if(foundItem) {
-      wishlistCtx.removeItem(item.id)
+      removeItem(item.id)
     }else {
-      wishlistCtx.addItem(item)
+      addItem(item)
     }
   }
 
@@ -111,19 +107,19 @@ import Slides from '../components/Slides';
     <>
     <Slides/>
    
-    <div id="section-2" className='mt-10 relative px-14 py-64' ref={sectionTwo}>
-      <div className="text-block hover:bg-black hover:text-white border-4 border-neutral-900 py-10 px-14 w-64 text-left absolute top-0 right-14 lg:right-[12%] lg:w-[350px] z-10 md:right-10">
+    <div id="section-2" className='mt-10 relative px-14 py-64' ref={sectionTwo}  >
+      <div className="text-block hover:bg-black hover:text-white border-4 border-neutral-900 py-10 px-14 w-64 text-left absolute bottom-0 lg:top-0 right-14 lg:right-[12%] lg:w-[350px] z-10 md:right-10">
           <span  className="block text-lg lg:text-xl font-bold lg:font-black uppercase">the concept</span>
           <h3 className="uppercase font-bold text-2xl lg:text-5xl">
             minimal frames intense colors dynamic lifestyle
           </h3>
       </div>
-      <div id="image-1" className="absolute top-0" >
-        <img src="/Cromier-HP-Banner-Concept.jpg" className='max-w-[800px] lg:mt-20'/>
+      <div id="image-1" className="absolute top-0 left-4 md:left-8 lg:left-14" >
+        <img src="/Cromier-HP-Banner-Concept.jpg" className='lg:max-w-[800px] sm:max-w-xs max-w-xs md:max-w-lg lg:mt-20'/>
       </div>
     </div>
   
-     <div className='px-4 grid md:grid-cols-2 mt-64 lg:mt-[500px] relative'>  
+     <div className='px-4 grid md:grid-cols-2 mt-10 sm:mt-48 md:mb-20 lg:mt-[500px] relative'>  
      <Swiper
         modules={[ Pagination, Autoplay ]}
         spaceBetween={50}
@@ -131,9 +127,9 @@ import Slides from '../components/Slides';
         loop
         autoplay
         pagination={{ clickable: true }}
-        className=" w-52 lg:w-96 md:w-68 md:my-0 md:mt-28 my-8 lg:absolute lg:-top-28 lg:left-[5%]"
+        className=" w-52 lg:w-96 md:w-68 md:my-0 md:mt-28 my-8 lg:absolute lg:-top-28 lg:left-[5%] md:order-first order-last lg:order-first"
         >
-        {menSneakers.slice(0, 10).filter((item: { id: number }) =>  item.id).map((product: any)=> (
+        {menSneakers.slice(0, 10).filter((item: { id: number }) =>  item.id).map((product)=> (
           <SwiperSlide key={product.id}>
             <div className="grid">
             <IconContext.Provider value={{style: {fontWeight: 800}, className:"h-6 w-6"}}>
@@ -149,7 +145,7 @@ import Slides from '../components/Slides';
               <div className="aspect-w-1 aspect-h-1 w-full h-40 overflow-hidden bg-white xl:aspect-w-7 xl:aspect-h-8">
                 <img
                   src={product.images?.cutOut}
-                  alt={product.imageAlt}
+                  alt={product.shortDescription}
                   className="h-full w-full object-cover object-center group-hover:opacity-75 p-3"
                 />
               </div>
@@ -163,7 +159,7 @@ import Slides from '../components/Slides';
         ))}
         </Swiper>
         
-        <div id="section-4" className='relative py-64 px-4'>
+        <div id="section-4" className='relative py-64 px-4 md:order-1'>
           <div className="text-right w-48 absolute top-0 md:top-48 md:-left-10 lg:-top-16 lg:-left-12 z-10">
             <motion.span variants={colorVariants} animate="visible" className="uppercase font-bold">fw22-23</motion.span>
             <motion.div variants={colorVariants} animate="visible" className="uppercase font-bold text-3xl md:text-5xl lg:text-5xl md:break-normal md:flex md:justify-end">men<br className='mr-12'/>collection</motion.div>
@@ -172,13 +168,13 @@ import Slides from '../components/Slides';
             </Link>
           </div>
           <div id="image" className='absolute bottom-0 right-4 pt-4'>
-            <img src={"/men.jpg"} className='max-w-[600px]'/>
+            <img src={"/men.jpg"} className='lg:max-w-[600px]'/>
           </div>
         </div>
       </div>
 
 
-    <div className='px-2 grid md:grid-cols-2 mt-64 mb-[36px]'>
+    <div className='px-2 grid md:grid-cols-2 lg:mt-64 mb-[36px] md:mt-10'>
        <Swiper
         modules={[ Pagination, Autoplay ]}
         spaceBetween={50}
@@ -188,7 +184,7 @@ import Slides from '../components/Slides';
         pagination={{ clickable: true }}
         className="w-52 md:w-68 my-8 md:my-0 md:mt-28 order-last lg:w-[450px] lg:flex lg:flex-col lg:justify-center lg:self-center"
         >
-        {womenSneakers.slice(0, 10).filter((item: { id: number }) =>  item.id).map((product: any)=> (
+        {womenSneakers.slice(0, 10).filter((item: { id: number }) =>  item.id).map((product)=> (
           <SwiperSlide key={product.id}>
             <div className="grid">
             <IconContext.Provider value={{style: {fontWeight: 800}, className:"h-6 w-6"}}>
@@ -204,7 +200,7 @@ import Slides from '../components/Slides';
               <div className="aspect-w-1 aspect-h-1 w-full h-40 overflow-hidden bg-white xl:aspect-w-7 xl:aspect-h-8">
                 <img
                   src={product.images?.cutOut}
-                  alt={product.imageAlt}
+                  alt={product.shortDescription}
                   className="h-full w-full object-contain object-center group-hover:opacity-75 p-3"
                 />
               </div>
@@ -227,7 +223,7 @@ import Slides from '../components/Slides';
         </Link>
         </div>
         <div id="image" className='absolute bottom-0 left-4 pt-4'>
-          <img src='/women.jpg' className='max-w-[500px]'/>
+          <img src='/women.jpg' className='lg:max-w-[500px]'/>
         </div>
       </div>
     </div>
@@ -245,8 +241,8 @@ import Slides from '../components/Slides';
 export async function getStaticProps() {
   // Fetch data from external API
   const [menSneakers, womenSneakers] = await Promise.all([
-    fetch('https://www.farfetch.com/ng/plpslice/listing-api/products-facets?page=1&view=60&sort=3&scale=282&pagetype=Shopping&rootCategory=Men&pricetype=FullPrice&c-category=137174', options).then(r => r.json()).then(result =>  result.listingItems.items),
-    fetch("https://www.farfetch.com/ng/plpslice/listing-api/products-facets?page=1&view=60&sort=3&scale=274&pagetype=Shopping&rootCategory=Women&pricetype=FullPrice&c-category=136310", options).then(r => r.json()).then(result =>  result.listingItems.items),
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}?page=1&view=20&sort=3&scale=282&pagetype=Shopping&rootCategory=Men&pricetype=FullPrice&c-category=137174`, options).then(r => r.json()).then(result =>  result.listingItems.items.map((item: Sneakers) => ({...item, quantity: 1, size: 38}))),
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}?page=1&view=20&sort=3&scale=274&pagetype=Shopping&rootCategory=Women&pricetype=FullPrice&c-category=136310`, options).then(r => r.json()).then(result =>  result.listingItems.items.map((item: Sneakers) => ({...item, quantity: 1, size: 38}))),
   ])
 
   // Pass data to the page via props
