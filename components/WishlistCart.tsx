850a6ad739a61e2ac61194995bf54b924ee49c5c
wisdom eshiet <wisdomeshiet65@gmail.com>
2023-09-14 21:15:29 +0100

    refactoring: inserted improved context api and type declaration

--- components/WishlistCart.tsx
+++ components/WishlistCart.tsx
@@ -2,20 +2,20 @@ import React, { useCallback, useContext } from "react"
 import { MdClose } from "react-icons/md";
 import { IconContext } from "react-icons";
 import Link from "next/link";
-import BagContext from '../store/bag_context'
+import { useBag } from '../context/BagContext';
+
 
 type Props = {
-  data: any[];
-  handleClick: (item: any) => void;
-  addToBag: (item: any) => void;
+  data: Sneakers[];
+  handleClick: (item: Sneakers) => void;
+  addToBag: (item: Sneakers) => void;
 } 
 
 const WishlistCart: React.FC<Props> = ( {data, handleClick, addToBag } ) => {
-  const bagCtx =  useContext(BagContext)
-  const bag = bagCtx.items
-
+  const {bag} = useBag()
+  
   const baggedItem = useCallback(
-    (item : any) =>  bag.findIndex((it: { id: any; }) => it.id == item.id) != -1
+    (item: Sneakers) =>  bag.findIndex((it: { id: number; }) => it.id == item.id) != -1
      ,
     [bag],
   )
@@ -25,7 +25,7 @@ const WishlistCart: React.FC<Props> = ( {data, handleClick, addToBag } ) => {
     <div className="bg-white">
       <div className="mx-auto max-w-2xl py-8 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8"> 
         <div className="grid grid-cols-2 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-          {data?.filter((item: { id: number }) =>  item.id).map((product: any) => (
+          {data?.filter((item: { id: number }) =>  item.id).map((product: Sneakers) => (
             <div key={product.id} className="grid">
             <IconContext.Provider value={{style: {fontWeight: 800}, className:"h-6 w-6"}}>
               <button className="relative justify-self-end p-2 w-9 h-9 rounded-full mt-1 mr-3 font-black z-10" onClick={() =>{ handleClick(product);}}>
@@ -40,7 +40,7 @@ const WishlistCart: React.FC<Props> = ( {data, handleClick, addToBag } ) => {
               <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8 grid">
                 <img
                   src={product.images?.cutOut}
-                  alt={product.imageAlt}
+                  alt={product.shortDescription}
                   className="h-full w-full object-cover object-center group-hover:opacity-75"
                 />
               </div>
