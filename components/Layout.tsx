8769d3ae35847e02846a12ee012647f0680903e8
wisdom eshiet <wisdomeshiet65@gmail.com>
2023-11-08 23:43:16 +0100

    refactored layout component

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,15 +2,17 @@ import React, { ReactElement } from "react";
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import styles from "../styles/Layout.module.css";
+
+
 function Layout({children }: {children: ReactElement}) {
 
   return(
     <div className={styles.layout}>
-    <Navbar/>
-      <main>
-        {children}
-      </main>
-    <Footer/>
+      <Navbar/>
+        <main>
+          {children}
+        </main>
+      <Footer/>
     </div>
   )
 }
