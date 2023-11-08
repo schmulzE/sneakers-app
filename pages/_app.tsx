eff950c120aa7951654fd489710a31f520132a38
wisdom eshiet <wisdomeshiet65@gmail.com>
2023-11-08 13:19:47 +0100

    honestly i have no clue on what i did there!

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,8 +8,22 @@ import BagProvider from '../context/BagContext'
 import { Session } from 'next-auth';
 import { SessionProvider } from "next-auth/react"
 
+import { useRouter } from 'next/router';
+import { useRef } from 'react';
+
+export const usePreviousRoute = () => {
+const router = useRouter();
+
+const ref = useRef<string | null>(null);
+
+router.events?.on('routeChangeStart', () => {
+  ref.current = router.asPath;
+});
+ return ref.current;
+};
 
 function MyApp({ Component, pageProps, }: AppProps<{session: Session}>) {
+
   return( 
     <ChakraProvider>
       <BagProvider>
