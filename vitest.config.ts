d06974ccf12bbf29bb9a0579f5c2882b4e571ab4
wisdom eshiet <wisdomeshiet65@gmail.com>
2024-07-02 00:50:21 +0100

    first commit of vitest config file

---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    include: ['**/*.test.tsx', '**/*.test.ts'],
+    setupFiles: "./tests/setup.ts",
+    globals: true
+  },
+});
